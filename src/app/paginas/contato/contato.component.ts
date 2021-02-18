5f30958f5270525c9bd012e08999b6018991b5d6
Marina Micas <marinamicas@gmail.com>
2021-02-17 23:41:04 -0300

    ajustando as imagens e inserindo os ícones da tela de Contato

--- src/app/paginas/contato/contato.component.ts
+++ src/app/paginas/contato/contato.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelopeSquare, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faWhatsappSquare, IconDefinition as IconDefinitionBrands } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contato',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.scss']
 })
 export class ContatoComponent implements OnInit {
+
+  public faEnvelopeSquare: IconDefinition = faEnvelopeSquare;
+  public faLinkedin: IconDefinitionBrands = faLinkedin;
+  public faWhatsappSquare: IconDefinitionBrands = faWhatsappSquare;
 
   constructor() { }
 
